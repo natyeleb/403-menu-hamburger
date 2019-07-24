030e302e7e2d72e64dc8f747ca72daac16835cde
natyele <natyelebertolanii@gmail.com>
2019-07-24 16:23:38 -0300

    adicionar menu hamburger

---
+++ script.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector("#hamburger")
+let menu = document.querySelector("menu")
+
+function  controlarMenu(){
+    menu.classList.toggle("aparecerMenu")
+}
+
+hamburger.onclick
